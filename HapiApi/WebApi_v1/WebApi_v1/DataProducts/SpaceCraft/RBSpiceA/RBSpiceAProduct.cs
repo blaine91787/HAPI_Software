0ac06a6961994318645177a7814d0092620885e5
Blaine Harris <blaine91787@gmail.com>
2018-07-25 17:00:58 -0500

    Updated the directory of basepath and moved rbspice csv files so they'd persist through commits

--- HapiApi/WebApi_v1/WebApi_v1/DataProducts/SpaceCraft/RBSpiceA/RBSpiceAProduct.cs
+++ HapiApi/WebApi_v1/WebApi_v1/DataProducts/SpaceCraft/RBSpiceA/RBSpiceAProduct.cs
@@ -10,7 +10,8 @@ namespace WebApi_v1.DataProducts.RBSpiceA
 {
     public class RBSpiceAProduct : IProduct, IConfigurable
     {
-        private readonly string _basepath = @"C:\Users\FTECS Account\Documents\GitHub\FTECS\HapiApi\WebApi_v1\WebApi_v1\obj\Debug\";
+        //private readonly string _basepath = @"C:\Users\FTECS Account\Documents\GitHub\FTECS\HapiApi\WebApi_v1\WebApi_v1\obj\Debug\";
+        private readonly string _basepath = @"C:\Users\blaine.harris\Documents\GitHub\FTECS\HapiApi\WebApi_v1\WebApi_v1\SCRecords\RBSPA\";
 
         //private string path = @"C:\Users\blaine.harris\Documents\BlainesProjects\WebApi_v1\WebApi_v1\obj\Debug\rbsp-a-rbspice_lev-0_Auxil_20121028_v1.1.1-00.csv";
         public string[] Header { get; set; }
