9cb4234287882acf04ac9355a665bce1e74885bb
Blaine Harris <blaine91787@hotmail.com>
2018-08-10 17:12:39 -0500

    more testing

--- HapiApi/ConsoleApp1/ConsoleApp1/Program.cs
+++ HapiApi/ConsoleApp1/ConsoleApp1/Program.cs
@@ -12,34 +12,38 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             XmlDocument xdoc = new XmlDocument();
-            xdoc.Load(@"C:\Users\blaine.harris\Documents\Github\FTECS\HapiApi\WebApi_v1\WebApi_v1\HapiDataProducts\HapiCatalog.xml");
+            xdoc.Load(@"C:\Users\unicornpuke\Documents\Github\FTECS\HapiApi\WebApi_v1\WebApi_v1\HapiDataProducts\HapiCatalog.xml");
 
-            XmlNodeList spacecraft = xdoc.SelectNodes("/catalog/missions/spacecraft");
+            XmlNodeList spacecraft = xdoc.GetElementsByTagName("spacecraft")[0].ChildNodes;
+
+            string t1 = "    ";
+            string t2 = "\t";
+            string t3 = "\t    ";
+            string t4 = "\t\t";
 
             foreach(XmlElement sc in spacecraft)
             {
-                if(sc.HasChildNodes) // check for spacecraft name
+                XmlNodeList levels = sc.ChildNodes;
+                foreach(XmlElement level in levels)
                 {
-                    XmlNodeList  = sc.ChildNodes;
-
-                    if(scname.HasChildNodes) // check for spacecraft levels available
+                    XmlNodeList products = level.ChildNodes;
+                    foreach(XmlElement product in products)
                     {
-                        XmlElement levels = scname.FirstChild;
-                        foreach (XmlElement product in levels)
-                        {
-                            Console.WriteLine(product.Name);
-                        }
-                    }
+                        Console.WriteLine(sc.Name + "_" + level.Name + "_" + product.Name + ":\n");
+                        if(product.GetElementsByTagName("productdescription")[0] != null)
+                            Console.WriteLine(t2 + product.GetElementsByTagName("productdescription")[0].Attributes["description"].Value);
 
-                    //if(type.HasChildNodes)
-                    //{
-                    //    foreach (XmlElement param in type)
-                    //    {
-                    //        Console.WriteLine(param.InnerText);
-                    //    }
-                    //}
+                        if (product.GetElementsByTagName("path")[0] != null)
+                            Console.WriteLine(t2 + product.GetElementsByTagName("path")[0].Attributes["path"].Value);
+
+                        XmlNodeList fields = product.GetElementsByTagName("field");
+                        foreach(XmlElement field in fields)
+                        {
+                            Console.WriteLine(t3 + field.InnerText);
+                        }
+                        Console.WriteLine();
+                    }
                 }
-                //Console.WriteLine(node);
             }
 
             Console.ReadKey();
